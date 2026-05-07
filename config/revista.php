62db8f2d7a7b57084906c8e819de1db1d0987f52
Felipe Calogero <felipe.augusto@bussolasocial.com.br>
2026-05-07 17:23:19 -0300

    changed the old url to new url

--- config/revista.php
+++ config/revista.php
@@ -12,11 +12,11 @@ return [
     |
     */
 
-    'edicoes_arquivo_url' => env('REVISTA_EDICOES_ARQUIVO_URL', 'https://catolicismo.com.br/'),
+    'edicoes_arquivo_url' => env('REVISTA_EDICOES_ARQUIVO_URL', 'https://acervo.catolicismo.com.br/'),
 
-    'edicoes_novas_url' => env('REVISTA_EDICOES_NOVAS_URL', 'https://novaversao.catolicismo.com.br/edicoes'),
+    'edicoes_novas_url' => env('REVISTA_EDICOES_NOVAS_URL', 'https://catolicismo.com.br/edicoes'),
 
     /** Buscador / listagem com foco em 2025+ (ano na query da listagem de edições) */
-    'edicoes_busca_url' => env('REVISTA_EDICOES_BUSCA_URL', 'https://novaversao.catolicismo.com.br/edicoes?year=2025'),
+    'edicoes_busca_url' => env('REVISTA_EDICOES_BUSCA_URL', 'https://catolicismo.com.br/edicoes?year=2025'),
 
 ];
